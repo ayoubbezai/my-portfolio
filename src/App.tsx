a5404c330b6aab56755b186fb052830ff42845dc
Ayoubbezai2024 <bezaiayoub8@gmail.com>
2024-12-21 19:33:14 +0100

    hero again

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import Hero from "./components/Hero.tsx"
 
 const App = () =>{
   return (
-    <div className=" fixed z-0 h-full w-full bg-slate-950">
-
-        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]">
-  <Navbar />
+    <div className=" m-0 p-0   h-full w-full bg-slate-950">
+        <div className=" m-0 p-0 absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]">
   </div>
+  <Navbar />
 
+  <Hero />
+  <Hero />
   <Hero />
   </div>
 
