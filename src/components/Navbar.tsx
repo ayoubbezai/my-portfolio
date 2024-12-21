095d9a46b41a12ec4bb637ab1c72b3fef383508c
Ayoubbezai2024 <bezaiayoub8@gmail.com>
2024-12-21 19:40:53 +0100

    hero again

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,9 +6,9 @@ import {FaFacebookSquare} from "react-icons/fa"
 const Navbar = () => {
   return (
     <nav className="flex flex-row justify-between text-center align-center  pt-6 relative">
-      <h1 className=" font-bold  text-xl text-neutral-200 first-letter:text-purple-500 md:text-2xl mx-8 bg-transparent self-center  lg:mx-16  xl:text-3xl xl:mx-24 ">
+      <h1 className=" font-bold  text-xl text-gray-400 first-letter:text-purple-500 md:text-2xl mx-8 bg-transparent self-center  lg:mx-16  xl:text-3xl xl:mx-24 ">
         AB</h1>
-       <div className="self-center justify-between text-center align-center text-xl text-neutral-200 md:text-xl flex space-x-2 lg:space-x-4 mx-8 lg:mx-16 xl:text-3xl xl:mx-24">
+       <div className="self-center justify-between text-center align-center text-xl text-gray-400 md:text-xl flex space-x-2 lg:space-x-4 mx-8 lg:mx-16 xl:text-2xl xl:mx-24">
         {/* LinkedIn */}
         <a
           href="https://www.linkedin.com/in/your-linkedin-profile"
