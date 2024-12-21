71319308617f1157d8655242d26f07375ae68e4c
Ayoubbezai2024 <bezaiayoub8@gmail.com>
2024-12-21 19:40:53 +0100

    hero again

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,8 +5,8 @@ const Hero: React.FC = () => {
     <div className="flex flex-col-reverse md:flex-row justify-evenly m-12  md:px-8 pt-8 md:m-24 md:mt-12 space-x-2 align-center  text-center md:text-left ">
       <div className=" ml-4">
         <h1 className="text-white text-5xl capitalize first-letter:text-purple-600 font-thin  mb-8 mt-4">Ayoub Bezai</h1>
-        <h2 className="mb-2 "><span className=" bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-2xl tracking-tight text-transparent">Front-end Developer </span></h2>
-        <p className=" text-gray-400 text-sm leading-normal w-full  md:w-[70%] 2xl:text:md ">{HERO_CONTENT}</p>
+        <h2 className="mb-2 "><span className=" bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-2xl md:text-3xl tracking-tight text-transparent">Front-end Developer </span></h2>
+        <p className=" text-gray-400 text-sm md:text-lg leading-normal w-full  md:w-[70%] 2xl:text:lg ">{HERO_CONTENT}</p>
       </div>
       <img src={image1} alt="Hero" className="  w-[60%] self-center md:w-[40%] z-50  md:mr-12" />
     </div>
