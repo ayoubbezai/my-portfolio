6cdf45da1f754a89b2a78f53c70fec579f535bf7
Ayoubbezai2024 <bezaiayoub8@gmail.com>
2024-12-21 17:22:53 +0100

    nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,9 +6,9 @@ import {FaFacebookSquare} from "react-icons/fa"
 const Navbar = () => {
   return (
     <nav className="flex flex-row justify-between text-center align-center  my-6">
-      <h1 className=" font-bold  text-xl text-neutral-200 first-letter:text-purple-500 md:text-2xl mx-8 bg-transparent self-center  lg:mx-16  xl:text-5xl xl:mx-24 ">
+      <h1 className=" font-bold  text-xl text-neutral-200 first-letter:text-purple-500 md:text-2xl mx-8 bg-transparent self-center  lg:mx-16  xl:text-3xl xl:mx-24 ">
         AB</h1>
-      <div className="self-center justify-between text-center align-center  text-xl text-neutral-200 md:text-xl flex space-x-2 lg:space-x-4 mx-8  lg:mx-16 xl:text-4xl xl:mx-24">
+      <div className="self-center justify-between text-center align-center  text-xl text-neutral-200 md:text-xl flex space-x-2 lg:space-x-4 mx-8  lg:mx-16 xl:text-3xl xl:mx-24">
         <FaLinkedin className=" hover:scale-105 lg:hover:-translate-y-1 cursor-pointer"/>
         <FaGithub className=" hover:scale-105 lg:hover:-translate-y-1 cursor-pointer" />
         <FaInstagramSquare  className=" hover:scale-105 lg:hover:-translate-y-1 cursor-pointer" />
