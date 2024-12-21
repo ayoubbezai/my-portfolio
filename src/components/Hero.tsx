b0e42338b8b90eaa141c72b45f5266f53b78489f
Ayoubbezai2024 <bezaiayoub8@gmail.com>
2024-12-21 21:47:32 +0100

    font

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero: React.FC = () => {
       <div className="z-50  ml-4">
         <h1 className="text-white text-5xl capitalize first-letter:text-purple-600 font-thin  mb-8 mt-4">Ayoub Bezai</h1>
         <h2 className="mb-2 "><span className=" bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-2xl md:text-3xl tracking-tight text-transparent">Front-end Developer </span></h2>
-        <p className=" text-gray-400 text-sm md:text-md  text-left leading-normal w-full  md:w-[70%] md:mt-4 lg:text:xl ">{HERO_CONTENT}</p>
+        <p className=" text-gray-400 text-sm md:text-base  text-left leading-normal w-full  md:w-[70%] md:mt-4 lg:text:xl ">{HERO_CONTENT}</p>
       </div>
       <img src={image1} alt="Hero" className="  w-[80%] self-center md:w-[40%] z-50  md:mr-12" />
     </div>
