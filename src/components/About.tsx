63a30918460edbc3d32d280fc645f57c75d67365
Ayoubbezai2024 <bezaiayoub8@gmail.com>
2024-12-21 22:02:40 +0100

    font

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,9 +6,9 @@ const About = () => {
     <div className="mt-32 ">
        <h1 className="text-gray-300 text-center text-3xl capitalize font-normal  mb-3  z-50">Ayoub <span className="text-gray-600 ">Me</span></h1>
 
-      <div className="flex flex-col-reverse md:space-x-24 md:flex-row  justify-between  m-4 mt-2 mb-0  md:px-8 pt-8 md:mx-24 md:my-0  align-center  text-center md:text-left ">
+      <div className="flex flex-col-reverse md:space-x-24 gap-x-11 md:flex-row  justify-between  m-4 mt-2 mb-0  md:px-8 pt-8 md:mx-24 md:my-0  align-center  text-center md:text-left ">
         <p className=" z-50 text-gray-400 text-sm text-left ml-3
-         md:text-base  leading-relaxed w-[90%]  md:w-[70%] md:mt-4 lg:text-xl">{ABOUT_TEXT}</p>
+           leading-relaxed w-[90%]  md:w-[70%] md:mt-4 2xl:text-xl">{ABOUT_TEXT}</p>
         <img src={image2} alt="about" className="  w-[80%] self-center md:w-[40%] z-50 mr   md:mr-12 mb-8 rounded-xl"  />
       </div>
     </div>
