d02f1f7fe27abcf0444c232d1b4219d7c7e14f2d
Ayoubbezai2024 <bezaiayoub8@gmail.com>
2025-01-13 02:34:10 +0100

    error handling

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 // Import images
 import project1 from "../assets/projects/project-1.png";
-import project2 from "../assets/projects/project-2.png";
+// import project2 from "../assets/projects/project-2.png";
 import project3 from "../assets/projects/project-3.png";
 import project4 from "../assets/projects/project-4.png";
 import project5 from "../assets/projects/project-5.png";
