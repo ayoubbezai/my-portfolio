4f2e04e41b1d513c3eb9acfd6995642ef933f913
ayoubbezai <bezaiayoub8@gmail.com>
2025-05-26 17:04:26 +0100

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ const App = () => {
       <div className="  fixed bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_100%_100%_at_50%_20%,#000_100%,transparent_100%)] dark:[mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
       <section id="alert">
         <Navbar setIsDarkMode={setIsDarkMode} isDarkMode={isDarkMode} />
-        <Hero isDarkMode={isDarkMode} />
+        <Hero/>
         <About />
         {/* <Events isDarkMode={isDarkMode} /> */}
         <Technologies />
